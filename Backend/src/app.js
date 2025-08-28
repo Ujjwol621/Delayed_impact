4c9da0b545808ab5ec531c14482ab1589d7c0c78
Kshitiz <kjdj802@gmail.com>
2025-08-29 01:03:44 +0545

    installed and configured "cors"

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,19 @@
 import connectDB from './data/db.js';
 import express from 'express';
+import cors from "cors";
 import countRoute from './router/counterRouter.js'
 import authRoute from './router/userRouter.js';
 import servicesRoute from './router/serviceRouter.js'
-import tokenRoute from './router/serviceRouter.js'
+import tokenRoute from './router/tokenRouter.js'
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/counter",countRoute);
 app.use("/api/services",servicesRoute);
-app.use("api/token",tokenRoute);
+app.use("/api/token",tokenRoute);
+
+
 
 connectDB();
 app.listen(8000, () => {
